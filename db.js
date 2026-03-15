fe2eec4f567e177b844e24dd6a52a783c9315f96
Udavkaa-ai <udavkaa2011@gmail.com>
2026-03-15 16:35:33 +0300

    Add files via upload

--- db.js
+++ db.js
@@ -1,6 +1,9 @@
 const Database = require('better-sqlite3');
 const path = require('path');
+const fs = require('fs');
 const dbPath = process.env.DB_PATH || path.join(__dirname, 'diary.db');
+const dbDir = path.dirname(dbPath);
+if (!fs.existsSync(dbDir)) fs.mkdirSync(dbDir, { recursive: true });
 console.log(`[db] path: ${dbPath}`);
 const db = new Database(dbPath);
 
